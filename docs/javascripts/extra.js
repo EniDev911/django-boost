e32c14a2c82fefeeafc8d161facd930e64deaf81
enidev911 <enidev911@gmail.com>
2025-02-24 01:34:36 -0300

    up: content

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -13,6 +13,17 @@
 // 	})
 // })
 
+String.prototype.slugify = function (separator = "-") {
+    return this
+        .toString()
+        .normalize('NFD') // split an accented letter in the base letter and the acent
+        .replace(/[\u0300-\u036f]/g, '') // remove all previously split accents
+        .toLowerCase()
+        .trim()
+        .replace(/[^a-z0-9 ]/g, '')  // remove all chars not letters, numbers and spaces (to be replaced)
+        .replace(/\s+/g, separator);
+};
+
 document.addEventListener('DOMContentLoaded', function () {
 	const alertButtons = document.querySelectorAll('.alerta');
 	const nameProjectTexts = document.querySelectorAll('.project_name');
@@ -51,11 +62,10 @@ document.addEventListener('DOMContentLoaded', function () {
 				}).then((platform) => {
 					if (platform.isConfirmed) {
 						nameProjectTexts.forEach(function (element) {
-							element.innerHTML = name_project.value;
+							element.innerHTML = name_project.value.slugify();
 						});
 						const isWindow = platform.value === 'window';
 						const venv_path = isWindow ? '.\\venv\\Scripts\\activate' : 'venv/bin/activate';
-						const source = isWindow ? 'none' : 'inline-block';
 						const commmand_source = isWindow ? { python:'python', activate: ''} : {python:'python3', activate: 'source '};
 						document.querySelector('.venv_path').textContent = venv_path;
 						document.querySelector('.command_so').textContent = commmand_source.python;
